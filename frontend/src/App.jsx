8d0262324d5e01dc13e8c098332db43e7472cba9
razibdash <avrorazib@gmail.com>
2025-07-29 09:26:29 +0600

    feat: update frontend dependencies and add button component
    - Added new dependencies: @radix-ui/react-slot, class-variance-authority, clsx, lucide-react, tailwind-merge, and others.
    - Removed postcss.config.js and replaced it with postcss.config.cjs for better compatibility.
    - Introduced a new Button component with variants using class-variance-authority.
    - Updated App component to include the new Button component.
    - Enhanced index.css with custom properties and dark mode support.
    - Configured Vite to resolve paths and added jsconfig paths support.
    - Created utility functions for class name management.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,13 @@
 import React from "react";
+import { Button } from "./components/ui/button";
 
 function App() {
-  return <div className="text-2xl font-bold">App</div>;
+  return (
+    <div className="text-2xl font-bold">
+      Welcome to The Mind Stack!
+      <Button className="cursor-pointer mt-4">Click Me</Button>
+    </div>
+  );
 }
 
 export default App;
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
-// https://vite.dev/config/
+import path from 'path'
+// import tailwindcssAnimate from 'tailwindcss-animate'
+import jsconfigPaths from 'vite-tsconfig-paths';
+
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [react(), tailwindcss(),  jsconfigPaths()],
+  resolve: {
+    alias: {
+      // eslint-disable-next-line no-undef
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
 })
+
+
