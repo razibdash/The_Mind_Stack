d657df63ab40d5dedb4c5de94132efc813371729
razibdash <avrorazib@gmail.com>
2025-07-30 01:21:56 +0600

    feat: add common form components and UI elements
    - Implemented FormControls component for dynamic form rendering based on control type (input, select, textarea).
    - Created CommonForm component to handle form submission and integrate FormControls.
    - Developed various UI components including Card, Carousel, Checkbox, Dropdown Menu, Input, Label, Select, Skeleton, Switch, Tabs, and Textarea.
    - Added configuration for sign-up and sign-in forms, including initial form data and control definitions.
    - Introduced course-related options and initial data structures for course landing pages and curriculum.

--- frontend/src/pages/auth/index.jsx
+++ frontend/src/pages/auth/index.jsx
@@ -1,7 +1,85 @@
-import React from "react";
+import CommonForm from "@/components/common-from";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import { GraduationCap, Image } from "lucide-react";
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
 
 function AuthIndex() {
-  return <div>Index</div>;
+  const [activeTab, setActiveTab] = useState("signin");
+  function handleTabChange(value) {
+    setActiveTab(value);
+  }
+  return (
+    <div className="flex flex-col min-h-screen">
+      <header className="px-4 lg:px-6 h-14 flex items-center border-b">
+        <Link to={"/"} className="flex items-center justify-center">
+          <img src="/logo1.PNG" className=" text-xl h-8 w-8 lg:h-10 lg:w-10" />
+          <span className=" mt-2 lg:text-xl">Mind Stack</span>
+        </Link>
+      </header>
+      <div className="flex  items-center justify-center min-h-screen bg-background">
+        <Tabs
+          value={activeTab}
+          defaultValue="signin"
+          onValueChange={handleTabChange}
+          className="w-full max-w-md"
+        >
+          <TabsList className="grid w-full grid-cols-2">
+            <TabsTrigger value="signin">Sign In</TabsTrigger>
+            <TabsTrigger value="signup">Sign Up</TabsTrigger>
+          </TabsList>
+          <TabsContent value="signin">
+            <Card className="p-6 space-y-4">
+              <CardHeader>
+                <CardTitle>Sign in to your account</CardTitle>
+                <CardDescription>
+                  Enter your email and password to access your account
+                </CardDescription>
+              </CardHeader>
+              <CardContent className="space-y-2">
+                <CommonForm
+                  formControls={signInFormControls}
+                  buttonText={"Sign In"}
+                  formData={signInFormData}
+                  setFormData={setSignInFormData}
+                  isButtonDisabled={!checkIfSignInFormIsValid()}
+                  handleSubmit={handleLoginUser}
+                />
+              </CardContent>
+            </Card>
+          </TabsContent>
+          <TabsContent value="signup"></TabsContent>
+          {/* <TabsContent value="signin">
+            <Card className="p-6 space-y-4">
+              <CardHeader>
+                <CardTitle>Sign in to your account</CardTitle>
+                <CardDescription>
+                  Enter your email and password to access your account
+                </CardDescription>
+              </CardHeader>
+              <CardContent className="space-y-2">
+                <CommonForm
+                  formControls={signInFormControls}
+                  buttonText={"Sign In"}
+                  formData={signInFormData}
+                  setFormData={setSignInFormData}
+                  isButtonDisabled={!checkIfSignInFormIsValid()}
+                  handleSubmit={handleLoginUser}
+                />
+              </CardContent>
+            </Card>
+          </TabsContent> */}
+        </Tabs>
+      </div>
+    </div>
+  );
 }
 
 export default AuthIndex;
