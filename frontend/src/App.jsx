5cf642f5ac2764956b2e62c410c2a0838d97bdb8
razibdash <avrorazib@gmail.com>
2025-08-04 01:39:09 +0600

    feat: implement route guard for authentication and add instructor/student dashboard pages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,24 @@
-import React from "react";
+import React, { use, useContext } from "react";
 import { Button } from "./components/ui/button";
 import { Route, Router, Routes } from "react-router-dom";
 import AuthIndex from "./pages/auth";
+import { AuthContext } from "./context/auth-context";
+import RouteGuard from "./components/route-guard";
 
 function App() {
+  const { auth } = useContext(AuthContext);
   return (
     <Routes>
-      <Route path="/auth" element={<AuthIndex />} />
+      <Route
+        path="/auth"
+        element={
+          <RouteGuard
+            authenticated={auth.authenticated}
+            user={auth.user}
+            element={<AuthIndex />}
+          />
+        }
+      />
     </Routes>
   );
 }
---
+++ frontend/src/pages/instructors/index.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function InstructorDashboardPage() {
+  return <div>InstructorDashboardPage</div>;
+}
+
+export default InstructorDashboardPage;
