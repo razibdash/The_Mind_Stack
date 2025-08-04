32d3fe69693ee9b34bbc2fab92e18acd3d778853
razibdash <avrorazib@gmail.com>
2025-08-04 14:57:24 +0600

    style: update sidebar styling and header in instructor dashboard

--- frontend/src/pages/instructors/index.jsx
+++ frontend/src/pages/instructors/index.jsx
@@ -35,12 +35,17 @@ function InstructorDashboardPage() {
   }
   return (
     <div className="flex h-full min-h-screen bg-gray-100">
-      <aside className="w-64 bg-white border-r shadow hidden md:block">
+      <aside className="w-64  border-r shadow hidden bg-[#3192C7] md:block">
         <div className="p-4">
-          <h2 className="font-bold text-lg text-[#3192C7]">
-            Instructor Dashboard
-          </h2>
-          <nav className="mt-4">
+          <div className="flex items-center justify-baseline space-x-1">
+            <img
+              src="/logo1.PNG"
+              alt="Logo"
+              className="w-18 h-18 object-contain"
+            />
+            <h1 className="font-bold text-2xl mt-2  text-white">Instructor</h1>
+          </div>
+          <nav className="mt-5">
             {menuItems.map((menuItem) => (
               <Button
                 className="w-full justify-start mb-2 cursor-pointer"
