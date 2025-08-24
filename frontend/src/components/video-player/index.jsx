0cbc64e70db16df82e8601b8f8472d6d2db2b174
razibdash <avrorazib@gmail.com>
2025-08-25 01:07:50 +0600

    add video controllers states

--- frontend/src/components/video-player/index.jsx
+++ frontend/src/components/video-player/index.jsx
@@ -1,6 +1,20 @@
-import React from "react";
+import React, { useRef, useState } from "react";
 import ReactPlayer from "react-player";
 const VideoPlayer = ({ width = "100%", height = "100%", url }) => {
+  const [playing, setPlaying] = useState(false);
+  const [volume, setVolume] = useState(0.5);
+  const [muted, setMuted] = useState(0.5);
+  const [played, setPlayed] = useState(0);
+  const [seeking, setSeeking] = useState(false);
+  const [isFullScreen, setIsFullScreen] = useState(false);
+  const [showControlls, setShowControlls] = useState(false);
+  const playerRef = useRef(null);
+  const playerContainerRef = useRef(null);
+  const controllsTimeOutRef = useRef(null);
+
+  const handlePlayAndPuse = () => {
+    setPlaying(!playing);
+  };
   return (
     <div>
       <ReactPlayer src={url} width={"100%"} height={"100%"} />
