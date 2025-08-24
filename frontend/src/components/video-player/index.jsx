74454ba26acb4228e40ee8fc9f77208570aa6659
razibdash <avrorazib@gmail.com>
2025-08-25 01:45:35 +0600

    add button for pause and play

--- frontend/src/components/video-player/index.jsx
+++ frontend/src/components/video-player/index.jsx
@@ -1,5 +1,8 @@
 import React, { useRef, useState } from "react";
 import ReactPlayer from "react-player";
+import { Slider } from "../ui/slider";
+import { Button } from "../ui/button";
+import { Pause, Play } from "lucide-react";
 const VideoPlayer = ({ width = "100%", height = "100%", url }) => {
   const [playing, setPlaying] = useState(false);
   const [volume, setVolume] = useState(0.5);
@@ -15,9 +18,62 @@ const VideoPlayer = ({ width = "100%", height = "100%", url }) => {
   const handlePlayAndPuse = () => {
     setPlaying(!playing);
   };
+  const handaleProgress = () => {};
+  const handleVolumeChange = () => {};
+  const handleSeekChange = () => {};
+  const handleSeekMouseUp = () => {};
+  const handlePlayAndPause = () => {};
   return (
-    <div>
-      <ReactPlayer src={url} width={"100%"} height={"100%"} />
+    <div
+      ref={playerContainerRef}
+      className={`reletive bg-gray-500 rounded-lg overflow-hidden shadow transition-all duration-300 case-in-out ${
+        isFullScreen ? "w-screen h-screen" : ""
+      }`}
+      style={(width, height)}
+    >
+      <ReactPlayer
+        ref={playerRef}
+        className="absolute top-0 left-0  "
+        playing={playing}
+        volume={volume}
+        muted={muted}
+        onProgress={handaleProgress}
+        src={url}
+        width={"100%"}
+        height={"100%"}
+      />
+      {showControlls && (
+        <div
+          className={`absolute bottom-0 left-0 right-0 bg-gray-800 bg-opacity-75 p-4 transition-opacity duration-300
+            ${showControlls ? "opacity-100 " : "opacity-0"}
+            `}
+        >
+          <Slider
+            value={[played * 100]}
+            max={100}
+            step={0.1}
+            onValueChange={(value) => handleSeekChange([value[0] / 100])}
+            onValueCommit={handleSeekMouseUp}
+            className="w-full mb-4"
+          />
+          <div className="flex items-center justify-between">
+            <div className="flex items-center space-x-2">
+              <Button
+                variant="ghost"
+                size="icon"
+                onClick={handlePlayAndPause}
+                className="text-white bg-transparent hover:text-white hover:bg-gray-700"
+              >
+                {playing ? (
+                  <Pause className="h-6 w-6" />
+                ) : (
+                  <Play className="h-6 w-6" />
+                )}
+              </Button>
+            </div>
+          </div>
+        </div>
+      )}
     </div>
   );
 };
